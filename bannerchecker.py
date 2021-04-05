b8a69569d487eecdf6024dfa28f23ef4f6a3bc61
Bagunbiade <bagunbiade@s.healthcare>
2021-04-05 00:18:58 -0400

    Just messing around with some more py scripts

--- bannerchecker.py
+++ bannerchecker.py
@@ -1,9 +1,25 @@
 import os, sys, socket
 
-## NEEDS TO FIX
-ip = "192.168.2.80"
-port = 22
-vuln_list="vuln_list.txt"
+
+def file_access():
+    if  len(sys.argv) == 2:
+        vuln_list = sys.argv[1]
+
+        if not os.path.isfile(vuln_list):
+            print(f"{vuln_list} is not in the path ")
+            exit(0)
+
+            if not os.access(vuln_list, os.R_OK):
+                print(f"{vuln_list} access is denied")
+                exit(0)
+
+        return vuln_list
+
+    else:
+        print(f"you have type an extra argument {sys.argv[2:]}, you need just 1 arg")
+        exit(0)
+
+
 
 def bannergrab(ip, port):
     try:
@@ -14,56 +30,38 @@ def bannergrab(ip, port):
         s.connect((ip, port))
 
         banner = s.recv(1024)
-        type(banner)
-        return banner
+
+        return str(banner)
 
     except Exception as e:
         print(f"ERROR checkout ---- {e}")
 
+
 def vulncheck(banner, vuln_list):
     with open(vuln_list) as f:
         for line in f.readlines():
-            print("line " + line.strip())
-            print("banner" )
             if line.strip('\n') in banner.strip('\n'):
-                #print(f"{line.strip()} is in the file")
-                #print("Server is Vuln: " + banner.strip('\n'))
-                print("STuff")
+                print(f"{line.strip()} is a vulnerability in the file")
+                print("The exact Server Vuln: " + banner.strip('\n') + f"is gotten from {vuln_list}")
 
-# def file_access():
-#     if  len(sys.argv) == 2:
-#         filename = sys.argv[1]
-
-#         if not os.path.isfile(filename):
-#             print(f"{filename} is not in the path ")
-#             exit(0)
-
-#             if not os.access(filename, os.R_OK):
-#                 print(f"{filename} access is denied")
-#                 exit(0)
-
-#         else:
-#             print(f"{filename} looks good")
-
-# def main():
-#     file_access()
-#     portlist = [22]
-#     #for suf in range(80):
-#     suf="80"
-#     ip = f'192.168.2.{suf}'
-#     print(ip)
-#     for port in portlist:
-#         print(f"{ip} {port}")
-#         bannergrab(ip, port)
-#         if banner:
-#             print(f"[+] {ip}: {banner}")
-#             vulncheck(banner, vuln_list)
-
-# main()
 
 def main():
-    banner = bannergrab(ip, port)
-    vulncheck(banner, vuln_list)
+
+    vuln_list = file_access()
+
+    portlist = [22]
     
+    for suf in range(80,81):
+        ip = f'192.168.2.{suf}'
+        for port in portlist:
+
+            print(f"Scanning ip: {ip} and port: {port}")
+            banner = bannergrab(ip, port)
+
+            if banner:
+                vulncheck(banner, vuln_list)
+            else:
+                print(f'[-] Usage: {str(sys.argv[0])} <vuln filename>')
+                exit(0)
 
 main()
